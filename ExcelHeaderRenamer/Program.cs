81e748b3bac1e073c7ba27bf637afeebd9491e04
AkshayHere <akshymohn@gmail.com>
2026-05-28 21:11:10 +0800

    Updated appsettings.json file for latest configuration

--- ExcelHeaderRenamer/Program.cs
+++ ExcelHeaderRenamer/Program.cs
@@ -19,11 +19,6 @@ class Program
             .CreateLogger();
 
         // Project root
-        //string projectRoot = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory)!
-        //    .Parent!
-        //    .Parent!
-        //    .Parent!
-        //    .FullName;
         string projectRoot = AppContext.BaseDirectory;
 
         // Assets folder
